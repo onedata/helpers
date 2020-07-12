f8be44f7726adfb3b21ceb729e43dfb5d6651820
Bartek Kryza <bkryza@gmail.com>
2020-07-12 18:40:28 +0200

    VFS-6528 Fixed possible deadlock in idle HTTP session pool

--- src/httpHelper.cc
+++ src/httpHelper.cc
@@ -504,7 +504,18 @@ folly::Future<HTTPSession *> HTTPHelper::connect(HTTPSessionPoolKey key)
     HTTPSession *httpSession{nullptr};
     decltype(m_idleSessionPool)::accessor ispAcc;
     m_idleSessionPool.find(ispAcc, key);
-    ispAcc->second.blockingRead(httpSession);
+    auto idleSessionAvailable = ispAcc->second.read(httpSession);
+
+    if (!idleSessionAvailable) {
+        LOG_DBG(1)
+            << "HTTP idle session connection pool empty - delaying request by "
+               "10ms. In case this message shows frequently, consider "
+               "increasing connectionPoolSize for the given storage.";
+        const auto kHTTPIdleSessionWaitDelay = 10ul;
+        return folly::makeFuture()
+            .delayed(std::chrono::milliseconds(kHTTPIdleSessionWaitDelay))
+            .then([this, key]() { return connect(key); });
+    }
 
     assert(httpSession != nullptr);
 
