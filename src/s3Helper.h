7d0314d679538cc306cf564e971b3d4f53441539
Bartek Kryza <bkryza@gmail.com>
2020-07-30 18:51:29 +0200

    VFS-6623 Enabled access to public buckets without credentials

--- src/s3Helper.h
+++ src/s3Helper.h
@@ -68,8 +68,8 @@ public:
         const auto &scheme = getParam(parameters, "scheme", "https");
         const auto &hostname = getParam(parameters, "hostname");
         const auto &bucketName = getParam(parameters, "bucketName");
-        const auto &accessKey = getParam(parameters, "accessKey");
-        const auto &secretKey = getParam(parameters, "secretKey");
+        const auto &accessKey = getParam(parameters, "accessKey", "");
+        const auto &secretKey = getParam(parameters, "secretKey", "");
         const auto version = getParam<int>(parameters, "signatureVersion", 4);
         const auto maximumCanonicalObjectSize = getParam<size_t>(parameters,
             "maximumCanonicalObjectSize", kDefaultMaximumCanonicalObjectSize);
