8377094faf157c27d87dda08c406a53ddb5ddd69
Bartek Kryza <bkryza@gmail.com>
2025-06-04 16:19:06 +0200

    VFS-12848 Added initial caching storage helper factory

---
+++ include/helpers/cachingStorageHelperCreator.h
@@ -0,0 +1,89 @@
+#ifndef HELPERS_CACHING_STORAGE_HELPER_CREATOR_H
+#define HELPERS_CACHING_STORAGE_HELPER_CREATOR_H
+
+#include "storageHelper.h"
+#include "storageHelperCreator.h"
+
+#include <folly/FBString.h>
+#include <tbb/concurrent_hash_map.h>
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+namespace one {
+namespace helpers {
+
+/**
+ * A wrapper around StorageHelperCreator that provides caching of created
+ * storage helpers. For the same combination of arguments and buffered flag, it
+ * will return the same storage helper instance instead of creating a new one.
+ */
+template <typename CommunicatorT> class CachingStorageHelperCreator {
+public:
+    explicit CachingStorageHelperCreator(
+        std::shared_ptr<StorageHelperCreator<CommunicatorT>> creator)
+        : m_creator{std::move(creator)}
+    {
+    }
+
+    /**
+     * Get or create a storage helper for the given arguments.
+     * If a storage helper was previously created with the same arguments,
+     * returns the cached instance. Otherwise creates a new one and caches it.
+     */
+    std::shared_ptr<StorageHelper> getStorageHelper(
+        const std::unordered_map<folly::fbstring, folly::fbstring> &args,
+        bool buffered)
+    {
+        // Create a cache key from args and buffered flag
+        auto key = createCacheKey(args, buffered);
+
+        typename CacheMap::accessor accessor;
+        if (m_cache.insert(accessor, key)) {
+            // Key wasn't in cache, create new storage helper
+            accessor->second = m_creator->getStorageHelper(args, buffered);
+        }
+
+        return accessor->second;
+    }
+
+private:
+    using CacheKey = std::string;
+    using CacheMap =
+        tbb::concurrent_hash_map<CacheKey, std::shared_ptr<StorageHelper>>;
+
+    /**
+     * Creates a unique cache key from storage helper arguments and buffered
+     * flag.
+     */
+    static CacheKey createCacheKey(
+        const std::unordered_map<folly::fbstring, folly::fbstring> &args,
+        bool buffered)
+    {
+        std::string key;
+
+        // Add all args to key in sorted order for consistency
+        std::vector<std::pair<folly::fbstring, folly::fbstring>> sortedArgs(
+            args.begin(), args.end());
+        std::sort(sortedArgs.begin(), sortedArgs.end());
+
+        for (const auto &arg : sortedArgs) {
+            key +=
+                arg.first.toStdString() + "=" + arg.second.toStdString() + ";";
+        }
+
+        // Add buffered flag
+        key += "buffered=" + std::to_string(buffered);
+
+        return key;
+    }
+
+    std::shared_ptr<StorageHelperCreator<CommunicatorT>> m_creator;
+    CacheMap m_cache;
+};
+
+} // namespace helpers
+} // namespace one
+
+#endif // HELPERS_CACHING_STORAGE_HELPER_CREATOR_H
---
+++ test/unit/cachingStorageHelperCreator_test.cc
@@ -0,0 +1,132 @@
+/**
+ * @file cachingStorageHelperCreator_test.cc
+ * @author Bartek Kryza
+ * @copyright (C) 2025 ACK CYFRONET AGH
+ * @copyright This software is released under the MIT license cited in
+ * 'LICENSE.txt'
+ */
+
+#undef BUILD_PROXY_IO
+
+#include "helpers/cachingStorageHelperCreator.h"
+#include "nullDeviceHelper.h"
+
+#include <folly/executors/IOThreadPoolExecutor.h>
+#include <gtest/gtest.h>
+
+#include <memory>
+
+using namespace ::testing;
+using namespace one::helpers;
+
+class MockCommunicator {
+public:
+    MockCommunicator() = default;
+    ~MockCommunicator() = default;
+
+    std::shared_ptr<folly::Executor> executor() { return {}; }
+};
+
+class CachingStorageHelperCreatorTest : public ::testing::Test {
+public:
+    void SetUp() override
+    {
+        m_executor = std::make_shared<folly::IOThreadPoolExecutor>(1);
+        m_nullDeviceFactory =
+            std::make_shared<NullDeviceHelperFactory>(m_executor);
+        m_storageHelperCreator =
+            std::make_shared<StorageHelperCreator<MockCommunicator>>(m_executor,
+                m_executor, m_executor, m_executor, m_executor, m_executor,
+                m_executor, m_executor, m_executor, m_executor);
+        m_cachingCreator =
+            std::make_shared<CachingStorageHelperCreator<MockCommunicator>>(
+                m_storageHelperCreator);
+    }
+
+    void TearDown() override { m_executor->join(); }
+
+protected:
+    MockCommunicator m_communicator;
+    std::shared_ptr<folly::IOThreadPoolExecutor> m_executor;
+    std::shared_ptr<NullDeviceHelperFactory> m_nullDeviceFactory;
+    std::shared_ptr<StorageHelperCreator<MockCommunicator>>
+        m_storageHelperCreator;
+    std::shared_ptr<CachingStorageHelperCreator<MockCommunicator>>
+        m_cachingCreator;
+};
+
+TEST_F(CachingStorageHelperCreatorTest, ShouldReturnSameHelperForSameArgs)
+{
+    // Given
+    std::unordered_map<folly::fbstring, folly::fbstring> args{
+        {"type", NULL_DEVICE_HELPER_NAME}, {"latencyMin", "0"},
+        {"latencyMax", "0"}, {"timeoutProbability", "0"}};
+    bool buffered = false;
+
+    // When
+    auto helper1 = m_cachingCreator->getStorageHelper(args, buffered);
+    auto helper2 = m_cachingCreator->getStorageHelper(args, buffered);
+
+    // Then
+    ASSERT_EQ(helper1, helper2);
+}
+
+TEST_F(CachingStorageHelperCreatorTest,
+    ShouldReturnDifferentHelpersForDifferentArgs)
+{
+    // Given
+    std::unordered_map<folly::fbstring, folly::fbstring> args1{
+        {"type", NULL_DEVICE_HELPER_NAME}, {"latencyMin", "0"},
+        {"latencyMax", "0"}, {"timeoutProbability", "0"}};
+
+    std::unordered_map<folly::fbstring, folly::fbstring> args2{
+        {"type", NULL_DEVICE_HELPER_NAME}, {"latencyMin", "10"},
+        {"latencyMax", "20"}, {"timeoutProbability", "0"}};
+
+    bool buffered = false;
+
+    // When
+    auto helper1 = m_cachingCreator->getStorageHelper(args1, buffered);
+    auto helper2 = m_cachingCreator->getStorageHelper(args2, buffered);
+
+    // Then
+    ASSERT_NE(helper1, helper2);
+}
+
+TEST_F(CachingStorageHelperCreatorTest,
+    ShouldReturnDifferentHelpersForDifferentBufferedFlag)
+{
+    // Given
+    std::unordered_map<folly::fbstring, folly::fbstring> args{
+        {"type", NULL_DEVICE_HELPER_NAME}, {"latencyMin", "0"},
+        {"latencyMax", "0"}, {"timeoutProbability", "0"}};
+
+    // When
+    auto helper1 = m_cachingCreator->getStorageHelper(args, true);
+    auto helper2 = m_cachingCreator->getStorageHelper(args, false);
+
+    // Then
+    ASSERT_NE(helper1, helper2);
+}
+
+TEST_F(CachingStorageHelperCreatorTest,
+    ShouldReturnSameHelperForSameArgsInDifferentOrder)
+{
+    // Given
+    std::unordered_map<folly::fbstring, folly::fbstring> args1{
+        {"type", NULL_DEVICE_HELPER_NAME}, {"latencyMin", "0"},
+        {"latencyMax", "0"}, {"timeoutProbability", "0"}};
+
+    std::unordered_map<folly::fbstring, folly::fbstring> args2{
+        {"timeoutProbability", "0"}, {"latencyMax", "0"}, {"latencyMin", "0"},
+        {"type", NULL_DEVICE_HELPER_NAME}};
+
+    bool buffered = false;
+
+    // When
+    auto helper1 = m_cachingCreator->getStorageHelper(args1, buffered);
+    auto helper2 = m_cachingCreator->getStorageHelper(args2, buffered);
+
+    // Then
+    ASSERT_EQ(helper1, helper2);
+}
