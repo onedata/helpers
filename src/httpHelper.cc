ecf8f54222b600eaa1a0eee466233e47efd4b6b5
Bartek Kryza <bkryza@gmail.com>
2026-06-05 15:07:21 +0200

    VFS-13614 Fixed handling of HTTP servers which return more data than requested in accept range request

--- src/httpHelper.cc
+++ src/httpHelper.cc
@@ -300,21 +300,15 @@ folly::Future<folly::IOBufQueue> HTTPFileHandle::read(const off_t offset,
                                -> folly::Future<folly::IOBufQueue> {
                     ONE_METRIC_TIMERCTX_STOP(timer, buf.chainLength());
 
-                    if (!getRequest->responseHasContentRange() ||
-                        !getRequest->responseHasContentLength()) {
+                    if (static_cast<size_t>(0) + offset > buf.chainLength())
+                        return makeFuturePosixException<folly::IOBufQueue>(
+                            ERANGE);
 
-                        if (static_cast<size_t>(0) + offset > buf.chainLength())
-                            return makeFuturePosixException<folly::IOBufQueue>(
-                                ERANGE);
-
-                        buf.trimStart(offset);
-                        folly::IOBufQueue res{
-                            folly::IOBufQueue::cacheChainLength()};
-                        res.append(std::move(buf).splitAtMost(size));
-                        return res;
-                    }
-
-                    return std::move(buf);
+                    buf.trimStart(offset);
+                    folly::IOBufQueue res{
+                        folly::IOBufQueue::cacheChainLength()};
+                    res.append(std::move(buf).splitAtMost(size));
+                    return res;
                 });
         });
 }
